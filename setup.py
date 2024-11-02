1bd3353eac617790330e48c2951ca9e27ced2245
kennypavan <kenpavan@gmail.com>
2024-11-02 13:20:47 -0700

    dependency updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='AnnSQL',
-    version='v0.9.7',
+    version='v0.9.8',
     author="Kenny Pavan",
     author_email="pavan@ohsu.edu",
     description="A Python SQL tool for converting Anndata objects to a relational DuckDb database. Methods are included for querying and basic single-cell preprocessing (experimental). ",
@@ -23,5 +23,8 @@ setuptools.setup(
     install_requires=[
         'scanpy>=1.10.3',
 		'duckdb>=1.1.2',
+		'memory-profiler>=0.61.0',
+		'psutil>=6.0.0',
+		'pyarrow>=17.0.0',
     ],
 )
