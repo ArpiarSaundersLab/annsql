dfcdc1d8606cab84714c26c8e3dbe45b966d58e9
kennypavan <kenpavan@gmail.com>
2025-01-24 15:01:25 -0800

    Added Splatter wrapper for in-silico tests

--- examples/analysis/staging.py
+++ examples/analysis/staging.py
@@ -3,18 +3,11 @@ import scanpy as sc
 import pandas as pd
 from AnnSQL import AnnSQL
 from AnnSQL.MakeDb import MakeDb
+from statsmodels.stats.multitest import multipletests
 import time
 
-
 #open the database
-asql = AnnSQL(db="../db/pbmc3k.asql")
-
+asql = AnnSQL(db="../db/random_data_1000.asql")
 asql.show_tables()
-
 asql.query("Describe X")
-
-
-# #filter the ENSMUSG00000070880 the same way as the query but in scanpy
-# start_time = time.time()
-
-# print("Filtering runtime no df:", time.time() - start_time)
+asql.query("Describe obs")
