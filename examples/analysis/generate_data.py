8ca7206e667632e0e3b29f92fe9b799961e1523f
kennypavan <kenpavan@gmail.com>
2025-01-24 16:54:15 -0800

    Splatter added to generate in-silico datasets for analysis

--- examples/analysis/generate_data.py
+++ examples/analysis/generate_data.py
@@ -9,6 +9,8 @@ import numpy as np
 import gc
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scsim import scsim
+
 
 #dataset sizes to generate
 dataset_sizes = [1000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 75000, 100000, 250000]
@@ -18,15 +20,30 @@ for i in dataset_sizes:
 
 	print(f"Running for {i}")
 
-	#the amount of columns (genes)
 	columns = 10000
 
+	simulator = scsim(ngenes=columns, ncells=i, ngroups=5, libloc=7.64, libscale=0.78,
+					mean_rate=7.68,mean_shape=0.34, expoutprob=0.00286,
+					expoutloc=6.15, expoutscale=0.49,
+					diffexpprob=0.025, diffexpdownprob=0., diffexploc=1.0, diffexpscale=1.0,
+					bcv_dispersion=0.448, bcv_dof=22.087, ndoublets=0,
+					nproggenes=400, progdownprob=0., progdeloc=1.0,
+					progdescale=1.0, progcellfrac=0.35, proggoups= list(range(1, int(10/3)+1)),
+					minprogusage=.1, maxprogusage=.7, seed=21)
+
+
+	start_time = time.time()
+	simulator.simulate()
+	end_time = time.time()
+	print('minutes elapsed for seed:',((end_time-start_time)/60))
+
 	#generate a cellxgene object using random data
-	adata = sc.AnnData(X=np.random.rand(i,columns), 
-						obs=pd.DataFrame(index=[f"cell_{i}" for i in range(i)]), 
-						var=pd.DataFrame(data=[f"gene_{i}" for i in range(columns)],columns=["gene_name"]))
+	adata = sc.AnnData(X=simulator.counts, 
+						obs=simulator.cellparams, 
+						)
 	adata.var.index = [f"gene_{i}" for i in range(adata.shape[1])]
-	adata.obs["cell_type"] = np.random.choice(["A","B","C"], adata.shape[0])
+	adata.obs["cell_type"] = adata.obs["group"]
+	adata.obs = adata.obs.drop(columns=["group","has_program","program_usage","libsize"])
 
 	#save the object
 	adata.write("../data/random_data_"+str(i)+".h5ad")
