60825ce62b98254ac8c743a1bc32c9dcaee0ebb6
kennypavan <kenpavan@gmail.com>
2025-03-12 19:49:08 -0700

    atlas profiling local results added

--- examples/analysis/atlas_dataset.py
+++ examples/analysis/atlas_dataset.py
@@ -85,34 +85,30 @@ def calculate_differential_expression_memory_wrapper():
 
 #open the database
 asql = AnnSQL(db="../db/Macosko_Mouse_Atlas_Processed.asql")
-# asql.query("SELECT ClusterNm,COUNT(ClusterNm) as total FROM obs GROUP BY ClusterNm")
-# asql.show_tables()
-# asql.query('SELECT * FROM diff_expression')
-
 
 #create a df to store the results
 # df = pd.DataFrame(columns=["function", "max_memory","runtime"])
-# df.to_csv("../results/atlas_runtime_memory.csv", index=False)
+# df.to_csv("atlas_profile.csv", index=False)
 
-# #simple filter
-# start_time = time.time()
-# result = memory_usage(test_filter)
-# runtime = time.time() - start_time
-# max_memory = max(result) - min(result)
-# print("test_filter", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
-# df = pd.concat([df, pd.DataFrame([["test_filter", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
-# df.to_csv("../results/atlas_runtime_memory.csv", index=False)
+#simple filter
+start_time = time.time()
+result = memory_usage(test_filter)
+runtime = time.time() - start_time
+max_memory = max(result) - min(result)
+print("test_filter", max_memory, runtime)
+df = pd.read_csv("../results/atlas_profile.csv")
+df = pd.concat([df, pd.DataFrame([["test_filter", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
+df.to_csv("../results/atlas_profile.csv", index=False)
 
-# #run total counts 
-# start_time = time.time()
-# result = memory_usage(calculate_total_counts_memory_wrapper)
-# runtime = time.time() - start_time
-# max_memory = max(result) - min(result)
-# print("calculate_total_counts_memory_wrapper", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
-# df = pd.concat([df, pd.DataFrame([["calculate_total_counts", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
-# df.to_csv("../results/atlas_runtime_memory.csv", index=False)
+#run total counts 
+start_time = time.time()
+result = memory_usage(calculate_total_counts_memory_wrapper)
+runtime = time.time() - start_time
+max_memory = max(result) - min(result)
+print("calculate_total_counts_memory_wrapper", max_memory, runtime)
+df = pd.read_csv("../results/atlas_profile.csv")
+df = pd.concat([df, pd.DataFrame([["calculate_total_counts", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
+df.to_csv("../results/atlas_profile.csv", index=False)
 
 # #run calculate_gene_counts NOTE: only gene counts (excluded mean when profiling)
 # start_time = time.time()
@@ -120,29 +116,29 @@ asql = AnnSQL(db="../db/Macosko_Mouse_Atlas_Processed.asql")
 # runtime = time.time() - start_time
 # max_memory = max(result) - min(result)
 # print("calculate_gene_counts_memory_wrapper", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
+# df = pd.read_csv("../results/atlas_profile.csv")
 # df = pd.concat([df, pd.DataFrame([["calculate_gene_counts", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
-# df.to_csv("../results/atlas_runtime_memory.csv", index=False)
+# df.to_csv("../results/atlas_profile.csv", index=False)
 
-# #run expression_normalize
-# start_time = time.time()
-# result = memory_usage(expression_normalize_memory_wrapper)
-# runtime = time.time() - start_time
-# max_memory = max(result) - min(result)
-# print("expression_normalize_memory_wrapper", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
-# df = pd.concat([df, pd.DataFrame([["expression_normalize", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
-# df.to_csv("../results/atlas_runtime_memory.csv", index=False)
+#run expression_normalize
+start_time = time.time()
+result = memory_usage(expression_normalize_memory_wrapper)
+runtime = time.time() - start_time
+max_memory = max(result) - min(result)
+print("expression_normalize_memory_wrapper", max_memory, runtime)
+df = pd.read_csv("../results/atlas_profile.csv")
+df = pd.concat([df, pd.DataFrame([["expression_normalize", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
+df.to_csv("../results/atlas_profile.csv", index=False)
 
-# #run expression_log
-# start_time = time.time()
-# result = memory_usage(expression_log_memory_wrapper)
-# runtime = time.time() - start_time
-# max_memory = max(result) - min(result)
-# print("expression_log_memory_wrapper", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
-# df = pd.concat([df, pd.DataFrame([["expression_log", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
-# df.to_csv("../results/atlas_runtime_memory.csv", index=False)
+#run expression_log
+start_time = time.time()
+result = memory_usage(expression_log_memory_wrapper)
+runtime = time.time() - start_time
+max_memory = max(result) - min(result)
+print("expression_log_memory_wrapper", max_memory, runtime)
+df = pd.read_csv("../results/atlas_profile.csv")
+df = pd.concat([df, pd.DataFrame([["expression_log", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
+df.to_csv("../results/atlas_profile.csv", index=False)
 
 # #run calculate_variable_genes
 # start_time = time.time()
@@ -150,8 +146,9 @@ asql = AnnSQL(db="../db/Macosko_Mouse_Atlas_Processed.asql")
 # runtime = time.time() - start_time
 # max_memory = max(result) - min(result)
 # print("calculate_variable_genes_memory_wrapper", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
+# df = pd.read_csv("../results/atlas_profile.csv")
 # df = pd.concat([df, pd.DataFrame([["calculate_variable_genes", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
+# df.to_csv("../results/atlas_profile.csv", index=False)
 
 # #run save_highly_variable_genes
 # start_time = time.time()
@@ -159,39 +156,10 @@ asql = AnnSQL(db="../db/Macosko_Mouse_Atlas_Processed.asql")
 # runtime = time.time() - start_time
 # max_memory = max(result) - min(result)
 # print("save_highly_variable_genes_memory_wrapper", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
+# df = pd.read_csv("../results/atlas_profile.csv")
 # df = pd.concat([df, pd.DataFrame([["save_highly_variable_genes", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
+# df.to_csv("../results/atlas_profile.csv", index=False)
 
-#TODO Start here
-#run calculate_pca
-start_time = time.time()
-result = memory_usage(calculate_pca_memory_wrapper)
-runtime = time.time() - start_time
-max_memory = max(result) - min(result)
-print("calculate_pca_memory_wrapper", max_memory, runtime)
-df = pd.read_csv("../results/atlas_runtime_memory.csv")
-df = pd.concat([df, pd.DataFrame([["calculate_pca", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
-df.to_csv("../results/atlas_runtime_memory.csv", index=False)
-
-# #run calculate_umap
-# start_time = time.time()
-# result = memory_usage(calculate_umap_memory_wrapper)
-# runtime = time.time() - start_time
-# max_memory = max(result) - min(result)
-# print("calculate_umap_memory_wrapper", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
-# df = pd.concat([df, pd.DataFrame([["calculate_umap", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
-# df.to_csv("../results/atlas_runtime_memory.csv", index=False)
-
-# #run calculate_leiden_clusters
-# start_time = time.time()
-# result = memory_usage(calculate_leiden_clusters_memory_wrapper)
-# runtime = time.time() - start_time
-# max_memory = max(result) - min(result)
-# print("calculate_leiden_clusters_memory_wrapper", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
-# df = pd.concat([df, pd.DataFrame([["calculate_leiden_clusters", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
-# df.to_csv("../results/atlas_runtime_memory.csv", index=False)
 
 # #run calculate_differential_expression
 # start_time = time.time()
@@ -199,7 +167,37 @@ df.to_csv("../results/atlas_runtime_memory.csv", index=False)
 # runtime = time.time() - start_time
 # max_memory = max(result) - min(result)
 # print("calculate_differential_expression_memory_wrapper", max_memory, runtime)
-# df = pd.read_csv("../results/atlas_runtime_memory.csv")
+# df = pd.read_csv("../results/atlas_profile.csv")
 # df = pd.concat([df, pd.DataFrame([["calculate_differential_expression", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
-# df.to_csv("../results/atlas_runtime_memory.csv", index=False)
+# df.to_csv("../results/atlas_profile.csv", index=False)
+
+# #run calculate_pca
+# start_time = time.time()
+# result = memory_usage(calculate_pca_memory_wrapper)
+# runtime = time.time() - start_time
+# max_memory = max(result) - min(result)
+# print("calculate_pca_memory_wrapper", max_memory, runtime)
+# df = pd.read_csv("atlas_profile.csv")
+# df = pd.concat([df, pd.DataFrame([["calculate_pca", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
+# df.to_csv("../results/atlas_profile.csv", index=False)
+
+# #run calculate_umap
+# start_time = time.time()
+# result = memory_usage(calculate_umap_memory_wrapper)
+# runtime = time.time() - start_time
+# max_memory = max(result) - min(result)
+# print("calculate_umap_memory_wrapper", max_memory, runtime)
+# df = pd.read_csv("atlas_profile.csv")
+# df = pd.concat([df, pd.DataFrame([["calculate_umap", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
+# df.to_csv("../results/atlas_profile.csv", index=False)
+
+# #run calculate_leiden_clusters
+# start_time = time.time()
+# result = memory_usage(calculate_leiden_clusters_memory_wrapper)
+# runtime = time.time() - start_time
+# max_memory = max(result) - min(result)
+# print("calculate_leiden_clusters_memory_wrapper", max_memory, runtime)
+# df = pd.read_csv("atlas_profile.csv")
+# df = pd.concat([df, pd.DataFrame([["calculate_leiden_clusters", max_memory, runtime]], columns=["function", "max_memory","runtime"])])
+# df.to_csv("../results/atlas_profile.csv", index=False)
 
